861f616169d8231f928787e28663eb53c066d12e
Eric Oestrich <eric@oestrich.org>
2011-10-25 12:38:12 -0400

    initial commit

---
+++ mongo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongo::Application.config.secret_token = '44adb95f60d5023e76b70a1fc0f4dd5998715e73dceb66cc053a16e6b4bc8b43cf1fa45b29b924f3084b479252255359a912918f1afa5add29978cb21530dc1d'
---
+++ relational/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relational::Application.config.secret_token = 'bfe2a85909d4bc746dac64d9fba6c1489ee5a3d1f81633bbda8b018b468f8d4a7ea0a3f6886d55106bd598a6d794bee9bb21011097741e2a4cb142ad1d83099e'
