2a9fd07a59f9a4ba491a6d79a20251f03eb2404d
Eric Oestrich <eric@oestrich.org>
2011-10-25 14:46:43 -0400

    bridge mongo and relational database

--- relational/test/integration/querying_test.rb
+++ relational/test/integration/querying_test.rb
@@ -9,13 +9,24 @@ class QueryingTest < ActionDispatch::IntegrationTest
     campaign2 = @user.campaigns.create(:name => "Customer Satisfaction Surveys")
 
     survey1 = campaign1.surveys.create(:name => "Wednesday Lunch")
-
-    @complete_response = survey1.responses.create(:complete => true)
+    # preexisting incomplete response
     survey1.responses.create(:complete => false)
+
+    # Here is a user completing their response
+    @complete_response = survey1.responses.create(:complete => true)
   end
 
   test "get_user_responses" do
     responses = Response.joins(:survey, :campaign).where('complete' => true, "campaigns.user_id" => @user.id)
     assert_equal [@complete_response], responses
+  end
+
+  test "operating on the answer collection" do
+    answer_collection = @complete_response.answer_collection
+
+    # answer_collection is a Mongoid document!!!
+    answer_collection.answers.create(:value => "I would like to go to McDonald's")
+
+    assert_equal @complete_response.answer_collection, AnswerCollection.find(@complete_response.bson_id)
   end
 end
